a5d790fbaeb501f5d72028bd81db226b34112303
Sudip Kumar <keya24gadu2020@gmail.com>
2024-05-08 22:49:54 -0800

    A

--- theatre_square.cpp
+++ theatre_square.cpp
@@ -1,12 +1,18 @@
+
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-
-   long long n, m, a;
-    scanf("%lld %lld %lld", &n, &m, &a);
-    long long area=ceil(n / a) * ceil(m / a);
-    cout<<area;
+    long long n, m, a;
+    cin >> n >> m >> a;
+    long long area = ceil(n*1.00 / a) * ceil(m*1.00 / a);
+    cout << area << endl;
     return 0;
 }
+
+
+
+
+
 
