ee638de964fab2ff32e6ac7ccc882b4dc39014f1
Sudip Kumar <keya24gadu2020@gmail.com>
2024-05-10 21:45:32 -0800

    div4

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,11 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
-int main()
-{
-
-   long long n, m, a;
-    scanf("%lld %lld %lld", &n, &m, &a);
-    long long area=ceil(n / a) * ceil(m / a);
-    printf("%lld\n", area);
-    return 0;
-}
+                // else
+                // count=1;
