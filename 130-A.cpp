74df61d4564e028220e268b1acbb7e4ff8b84a01
Sudip Kumar <keya24gadu2020@gmail.com>
2024-06-19 23:09:04 -0800

    A

--- 130-A.cpp
+++ 130-A.cpp
@@ -8,23 +8,36 @@ using namespace std;
 int main()
 {
     fio;
-    string s, res;
+    string s;
     cin >> s;
-    vector<string> v;
-    for (int i = 0; i < s.size(); ++i)
-    {
-        string sb = s.substr(0,i);
-        if (sb != "WUB")
-            v.push_back(sb);
+
+    // Replace all occurrences of "WUB" with a space
+    string::size_type pos = 0;
+    while ((pos = s.find("WUB", pos)) != string::npos) {
+        s.replace(pos, 3, " ");
+        pos += 1;  // Move to the next character to avoid infinite loop
     }
-    for (int i = 0; i < v.size(); ++i)
-    {
-        if (i > 0)
-        {
-            res += " ";
-            res += v[i];
+
+    // Trim leading and trailing spaces
+    s.erase(0, s.find_first_not_of(" "));
+    s.erase(s.find_last_not_of(" ") + 1);
+
+    // Remove multiple spaces
+    string res;
+    bool space = false;
+    for (char c : s) {
+        if (c == ' ') {
+            if (!space) {
+                res += c;
+                space = true;
+            }
+        } else {
+            res += c;
+            space = false;
         }
     }
+
     cout << res << endl;
     return 0;
 }
+
